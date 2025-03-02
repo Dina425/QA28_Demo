4c372bdbf8fd365496ebb02cbe7690935cbe20a5
Dina425 <bazhenovadina@gmail.com>
2025-03-02 13:23:12 +0200

    Exceptions class was added

--- src/main/java/experiments/Exeptions.java
+++ src/main/java/experiments/Exeptions.java
@@ -1,4 +1,33 @@
 package experiments;
 
 public class Exeptions {
+
+    public static void main(String[] args) {
+        String[] strArray={"str1","str2","str3"};
+        uncheckedExceptions(strArray);
+        try {
+            pause();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private static void uncheckedExceptions(String[] strArray){
+        try {
+            for (int i = 0; i < 5; i++) {
+                System.out.println(strArray[i]);
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+           e.printStackTrace();
+            System.out.println("created exception");
+        }
+        System.out.println("Program is working");
+    }
+    private static void pause() throws InterruptedException {
+        checkException();
+        System.out.println("Pause 3 sec");
+    }
+    private static void checkException() throws InterruptedException {
+        Thread.sleep(3000);
+    }
+
 }
